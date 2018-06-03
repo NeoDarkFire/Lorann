0caac3e9030d90933b6bdbc29fbbe2b859f65426
NeoDarkFire <dark.fire@gmx.fr>
2018-06-04 00:49:13 +0200

    implemented SpellAISystem.

--- lorann/controller/src/main/java/controller/SpellAISystem.java
+++ lorann/controller/src/main/java/controller/SpellAISystem.java
@@ -1,18 +1,44 @@
 package controller;
 
+import java.awt.Point;
+
 import ecs.Engine;
-import model.components.MoveComponent;
-import model.components.PositionComponent;
-import model.components.SpellAIComponent;
+import ecs.Entity;
+import model.components.*;
+import view.Action;
+import view.IView;
 
 public class SpellAISystem extends CustomSystem{
 	{
 		targets.add(SpellAIComponent.class);
 		targets.add(MoveComponent.class);
-		targets.add(PositionComponent.class);
+		targets.add(InputComponent.class);
+	}
+	
+	private boolean listen;
+	
+	public void onEntitAdded(final Entity e) {
+		this.listen = false;
 	}
 	
 	public void update(Engine engine, int dt) {
-		
+		final IView view = this.controller.getView();
+		if (this.listen && view.isPressed(Action.SPELL)) {
+			this.listen = false;
+			
+			final Entity[] targets = new Entity[1];
+			engine.getEntitiesWithComponents(PlayerComponent.class, PositionComponent.class).toArray(targets);
+			final Entity target = targets[0];
+			
+			if (target != null) {
+				final Point targetPos = targets[0].get(PositionComponent.class).pos; 
+				for (final Entity e : this) {
+					e.get(MoveComponent.class).movement.setDirection(targetPos);
+				}
+			}
+		}
+		else {
+			this.listen = true;
+		}
 	}
 }
--- lorann/controller/src/main/java/controller/UserInputSystem.java
+++ lorann/controller/src/main/java/controller/UserInputSystem.java
@@ -1,13 +1,13 @@
 package controller;
 
 import ecs.Engine;
-import model.components.InputComponent;
-import model.components.MoveComponent;
+import model.components.*;
 
 public class UserInputSystem extends CustomSystem {
 	{
 		targets.add(InputComponent.class);
 		targets.add(MoveComponent.class);
+		excluded.add(SpellAIComponent.class);
 	}
 	
 	public void update(Engine engine, int dt) {
