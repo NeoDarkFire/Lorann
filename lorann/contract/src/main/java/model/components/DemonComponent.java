c4e001e232d9874cb2a5bb174948ffed1b2a831e
aurel <aurel@DESKTOP-FKP4TOQ>
2018-06-05 22:44:56 +0200

    correction javadoc

--- lorann/contract/src/main/java/model/components/DemonComponent.java
+++ lorann/contract/src/main/java/model/components/DemonComponent.java
@@ -3,7 +3,7 @@ package model.components;
 import ecs.Component;
 
 /**
- * <h1>The Entity has a deamon</h1>
+ * <h1>The Entity has a daemon</h1>
  *
  */
 public class DemonComponent implements Component {
